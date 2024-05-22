e1ceb06513807be766db6a693e65b0e6deebefbe
jianggb <jianggb@miligc.com>
2024-05-22 12:35:58 +0800

    fix: improve tree gen

--- thinkgos/leet_03/leet_03.go
+++ thinkgos/leet_03/leet_03.go
@@ -1,103 +1,43 @@
 package leet_03
 
+type Node interface {
+	GetId() int
+	GetPid() int
+	AppendChildren(Node)
+}
+
+func IntoTree[T Node](rows []T, rootPid int) []T {
+	var root []T
+	nodes := make(map[int]T, len(rows))
+	for _, v := range rows {
+		node := v
+		id := node.GetId()
+		pid := node.GetPid()
+		if pid == rootPid {
+			root = append(root, node)
+		} else if parent, exists := nodes[pid]; exists {
+			parent.AppendChildren(node)
+		}
+		nodes[id] = node
+	}
+	return root
+}
+
 type Dept struct {
-	Id   int
-	Pid  int
-	Name string
+	Id       int
+	Pid      int
+	Name     string
+	Children []*Dept
 }
 
-type DeptTree struct {
-	*Dept
-	Children []DeptTree
+func (d *Dept) GetId() int {
+	return d.Id
 }
 
-func toDeptMap(rows []Dept) map[int][]*Dept {
-	mp := make(map[int][]*Dept)
-	for i := 0; i < len(rows); i++ {
-		v := &rows[i]
-		mp[v.Pid] = append(mp[v.Pid], v)
-	}
-	return mp
+func (d *Dept) GetPid() int {
+	return d.Pid
 }
 
-func toTree(rows []Dept, pid int) []DeptTree {
-	mp := toDeptMap(rows)
-	rt, ok := mp[pid]
-	if !ok || len(rt) == 0 {
-		return []DeptTree{}
-	}
-	root := make([]DeptTree, 0, len(rt))
-	for _, itm := range rt {
-		root = append(root, deepChildrenDept(mp, DeptTree{Dept: itm}))
-	}
-	return root
-}
-
-func deepChildrenDept(mp map[int][]*Dept, itm DeptTree) DeptTree {
-	children, ok := mp[itm.Id]
-	if ok {
-		itm.Children = make([]DeptTree, 0, len(children))
-		for _, v := range children {
-			itm.Children = append(itm.Children, deepChildrenDept(mp, DeptTree{Dept: v}))
-		}
-	} else {
-		itm.Children = []DeptTree{}
-	}
-	return itm
-}
-
-/**
-function toTree(arr, pid) {
-    let tree = [];
-    let hash = {};
-    arr.forEach((item) => {
-        const id = item.id;
-        item["children"] = hash[id] = [];
-        // 判断是否为父节点
-        if (item.pid === pid) {
-            tree.push(item);
-        } else {
-            const parentId = item.pid;
-            if (hash[parentId]) {
-                hash[parentId].push(item);
-            } else {
-                hash[parentId] = [];
-                hash[parentId].push(item);
-            }
-        }
-    });
-    console.log(hash)
-    return tree;
-}
-*/
-
-type DeptTree2 struct {
-	*Dept
-	Children *[]*DeptTree2
-}
-
-func toTree2(rows []Dept, pid int) []*DeptTree2 {
-	mp := make(map[int]*[]*DeptTree2) // 孩子
-	root := make([]*DeptTree2, 0, 10)
-	for i := 0; i < len(rows); i++ {
-		id := rows[i].Id
-		parentId := rows[i].Pid
-
-		dp := make([]*DeptTree2, 0)
-		mp[id] = &dp
-
-		entry := &DeptTree2{Dept: &rows[i], Children: &dp}
-		if parentId == pid {
-			root = append(root, entry)
-		} else {
-			v, ok := mp[parentId]
-			if ok {
-				*v = append(*v, entry)
-			} else {
-				dp = []*DeptTree2{entry}
-				mp[parentId] = &dp
-			}
-		}
-	}
-	return root
+func (d *Dept) AppendChildren(v Node) {
+	d.Children = append(d.Children, v.(*Dept))
 }
--- thinkgos/leet_03/leet_03_test.go
+++ thinkgos/leet_03/leet_03_test.go
@@ -7,40 +7,28 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-var arr = []Dept{
-	{1, 0, "超然科技"},
-	{2, 0, "低速科技"},
-	{3, 1, "科研中心"},
-	{4, 1, "运营中心"},
-	{5, 2, "吃喝院"},
-	{6, 2, "研究院"},
-	{7, 3, "aa"},
-	{8, 3, "bb"},
-	{9, 4, "cc"},
-	{10, 5, "dd"},
-	{11, 6, "ee"},
+var arr = []*Dept{
+	{1, 0, "超然科技", nil},
+	{2, 0, "低速科技", nil},
+	{3, 1, "科研中心", nil},
+	{4, 1, "运营中心", nil},
+	{5, 2, "吃喝院", nil},
+	{6, 2, "研究院", nil},
+	{7, 3, "aa", nil},
+	{8, 3, "bb", nil},
+	{9, 4, "cc", nil},
+	{10, 5, "dd", nil},
+	{11, 6, "ee", nil},
 }
 
 func TestTree(t *testing.T) {
-	v, err := json.MarshalIndent(toTree(arr, 0), " ", "  ")
+	v, err := json.MarshalIndent(IntoTree(arr, 0), " ", "  ")
 	require.NoError(t, err)
 	t.Logf(string(v))
 }
 
 func BenchmarkTree(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		toTree(arr, 0)
-	}
-}
-
-func TestTree2(t *testing.T) {
-	v, err := json.MarshalIndent(toTree2(arr, 0), " ", "  ")
-	require.NoError(t, err)
-	t.Logf(string(v))
-}
-
-func BenchmarkTree2(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		toTree2(arr, 0)
+		IntoTree(arr, 0)
 	}
 }
